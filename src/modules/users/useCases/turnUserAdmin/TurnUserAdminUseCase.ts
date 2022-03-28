1e01baa7612fa0be2764593d5329ff850da6ff25
Vinicius Meneses <vinicius.meneses@tagview.com.br>
2022-03-28 00:06:23 -0300

    Transforma usuário normal em administrador

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,9 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    if (!user) throw new Error("User doesn't exists");
+    return this.usersRepository.turnAdmin(user);
   }
 }
 
